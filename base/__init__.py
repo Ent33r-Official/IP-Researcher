ba9b793bb0086171a62b897ba0811a18899eb8d4
Ent33r-Official <61850501+Ent33r-Official@users.noreply.github.com>
2020-03-06 15:59:52 +0330

    Update __init__.py

--- base/__init__.py
+++ base/__init__.py
@@ -2,15 +2,10 @@ from telegram.ext import Updater
 import logging
 import os
 
-TOKEN = '1084877238:AAFpKmuVcjsfUx-ZJOr_segR-EW72RdxIdQ'
+TOKEN = 'TOKEN'
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO)
 
 up = Updater(TOKEN, use_context=True)
-
-def importing():
-    for i in os.listdir('base/modules/'):
-        __import__('base.modules.{}'.format(i.split('.')[0]))
-        print(i)
